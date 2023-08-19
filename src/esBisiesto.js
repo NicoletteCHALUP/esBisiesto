71b93ae826062f5abbce5e3821df3b1f414e1b6e
Nicolette <nicolette.chalup@ucb.edu.bo>
2023-08-19 16:09:36 -0400

    Toma en cuenta que 1700  es anio Bisiesto ya que Todos los años divisibles por 100 pero no por 400 NO son años bisiestos

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -2,6 +2,12 @@ function esBisiesto(anio){
     if(anio==4){
         return true;
     }
+    if(anio%400==0){
+      return true;  
+    }
+    if(anio%100==0 && anio%400!=0){
+        return false;  
+    }
     return false;
   }
  
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -9,8 +9,13 @@ describe("Es Bisiesto ", () => {
   it("El 5 no deberia considerarse como anio bisiesto", () => {
     expect(esBisiesto(5)).toEqual(false);
  });
+
  it("El 2000 deberia considerarse como anio bisiesto Todos los años divisibles por  400 SON años bisiestos ", () => {
   expect(esBisiesto(2000)).toEqual(true);
-});
+ });
+  it("El 1700 no deberia considerarse como anio bisiesto ya que Todos los años divisibles por 100 pero no por 400 NO son años bisiestos", () => {
+    expect(esBisiesto(1700)).toEqual(false);
+ });
+
 });
 
